02d0925b83a0db7be7d3092e3ca9a37c213bb9b6
Witek <witek.cynk06@gmail.com>
2024-04-24 12:47:21 +0200

    dupa

--- js.js
+++ js.js
@@ -1,6 +1,14 @@
 function Add(){
 
     const task = document.getElementById('name').value;
-    document.getElementById('wynik').innerHTML = task;
+    const status = document.getElementById('status').value;
+   
+    const obj = {name: task, status: status};
+
+    const tab = [ Object.values(obj)];
+    
+    console.log(tab);
+
+    
 
 }
