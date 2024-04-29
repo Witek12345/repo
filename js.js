4d4f90ed0a4aede77e361ac4f7475df09a0e8fc4
Dawid Bulinski <dawid.bulinski@xebia.com>
2024-04-29 20:41:20 +0200

    Event handling

--- js.js
+++ js.js
@@ -1,14 +1,21 @@
-function Add(){
+// Zapisywac w tablicy wszystkie taski
+// Renderowanie w liście
+// Dodac usuwanie taskow za pomocą filter
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+const tasks = [];
 
-    const task = document.getElementById('name').value;
-    const status = document.getElementById('status').value;
-   
-    const obj = {name: task, status: status};
+function handleButtonClick(event){
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-    const tab = [ Object.values(obj)];
-    
-    console.log(tab);
+    const task = formData.get('name')
+    const status = formData.get('status');
 
-    
+    const obj = { name: task, status: status };
+    tasks.push(obj);
 
+    event.target.reset();
 }
+
+// submit, click, input, change
+document.querySelector('.add-task-form').addEventListener('submit', handleButtonClick)
